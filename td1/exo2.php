2a4e62c4e108efde03f5bc67c4604ef926a91517
Thibault716 <62308553+Thibault716@users.noreply.github.com>
2020-03-18 15:35:14 +0100

    TD

--- td1/exo2.php
+++ td1/exo2.php
@@ -13,8 +13,8 @@
 </form>
 <body>
     <a href="?Color=%23ff0000&size=15&message=salut">Message 1</a>
-    <a href="?Color=%23ff0000&size=15&message=c'est">Message 2</a>
-    <a href="?Color=%231ae633&size=60&message=l'exercice 2">Message 3</a>
+    <a href="?Color=%230000FF&size=30&message=c'est">Message 2</a>
+    <a href="?Color=%231ae633&size=50&message=l'exercice 2">Message 3</a>
     <?php
     echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
     ?>
