58ba275da66f3212ca678cf776ab90faaa7cdd79
Thibault716 <62308553+Thibault716@users.noreply.github.com>
2020-03-19 11:09:38 +0100

    TD

--- td1/exo3.php
+++ td1/exo3.php
@@ -5,16 +5,18 @@
     <title>Hello</title>
 </head>
 <form method="GET">
-    <label for="color">Color : </label>
+    <label for="color">Couleur : </label>
     <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
-    <label for="size">Size : </label>
+    <label for="size">Taille : </label>
     <input type="number" value="" name="size" id="size">
+    <label for="contenue">A toi de jouer</label>
+    <input type="text" value="" name="message" id="message">
     <input type="submit" value="Valider">
 </form>
 <body>
-<a href="?Color=%23ff0000&size=15&message=salut">Message 1</a>
-<a href="?Color=%23ff0000&size=15&message=ça">Message 2</a>
-<a href="?Color=%231ae633&size=60&message=benji">Message 3</a>
+    <a href="?Color=%23ff0000&size=15&message=salut">Message 1</a>
+    <a href="?Color=%230000FF&size=30&message=c'est">Message 2</a>
+    <a href="?Color=%231ae633&size=50&message=l'exercice 2">Message 3</a>
     <?php
     echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
     ?>
