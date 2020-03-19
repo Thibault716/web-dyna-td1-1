973d09302b3e56f24a56479588609cc29ec484f2
Thibault716 <62308553+Thibault716@users.noreply.github.com>
2020-03-19 11:44:49 +0100

    TD

--- td1/exo3.php
+++ td1/exo3.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Hello</title>
 </head>
-<form method="GET">
+<form method="POST">
     <label for="color">Couleur : </label>
     <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
     <label for="size">Taille : </label>
@@ -16,9 +16,18 @@
 <body>
     <a href="?Color=%23ff0000&size=15&message=salut">Message 1</a>
     <a href="?Color=%230000FF&size=30&message=c'est">Message 2</a>
-    <a href="?Color=%231ae633&size=50&message=l'exercice 2">Message 3</a>
+    <a href="?Color=%231ae633&size=50&message=l'exercice 2">Message 3</a><br>
+    
     <?php
-    echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
+    if (empty($_POST)) {
+        echo "Euh, je crois que tu as oublié de remplir :D";
+    }
+    if (empty($_POST["size"]) and $_POST["Color"]) {
+        echo "<div style='font-size: 12px; color: black'>{$_POST["message"]}</div>";
+    }
+    else {
+        echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
+    }
     ?>
 </body>
 </html>
