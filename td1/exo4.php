126f240fcf289877fa988c589774c21eaf0a9c02
Thibault716 <62308553+Thibault716@users.noreply.github.com>
2020-03-26 16:35:59 +0100

    TD

--- td1/exo4.php
+++ td1/exo4.php
@@ -4,16 +4,16 @@
     <title>Exercice 4</title>
 </head>
 <body>
-    <a href="?nblignes=2&nbColonnes=2">2x2</a>
-    <a href="?nblignes=5&nbColonnes=5">5x5</a>
-    <a href="?nblignes=10&nbColonnes=8">10x8</a>
+    <a href="?ligne=2&colonne=2">2x2</a>
+    <a href="?ligne=5&colonne=5">5x5</a>
+    <a href="?ligne=10&colonne=8">10x8</a>
 
     <?php
     echo "<table border='1'>
     <tbody>";
-    for($l=0; $l<=$_GET["nblignes"]; $l++){ 
+    for($l=0; $l<=$_GET["ligne"]; $l++){ 
         echo "<tr>"; 
-        for($c=0; $c<=$_GET["nbColonnes"]; $c++){ 
+        for($c=0; $c<=$_GET["colonne"]; $c++){ 
             echo "<td align='center'>$l-$c</td>"; 
         } 
         echo "</tr>"; 
