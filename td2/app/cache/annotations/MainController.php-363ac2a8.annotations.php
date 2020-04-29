bcc53f10fc6a76dd813df597e8277d1f568347a2
Thibault716 <62308553+Thibault716@users.noreply.github.com>
2020-04-29 16:06:48 +0200

    TD

--- td2/app/cache/annotations/MainController.php-363ac2a8.annotations.php
+++ td2/app/cache/annotations/MainController.php-363ac2a8.annotations.php
@@ -3,25 +3,23 @@
 return array(
   '#namespace' => 'controllers',
   '#uses' => array (
+  'AbstractWsController' => 'ws\\controllers\\AbstractWsController',
 ),
   '#traitMethodOverrides' => array (
   'controllers\\MainController' => 
   array (
   ),
 ),
-  'controllers\\MainController::index' => array(
+  'controllers\\MainController::contact' => array(
     array('#name' => 'get', '#type' => 'Ubiquity\\annotations\\router\\GetAnnotation', "_default","name"=>"Home")
   ),
-  'controllers\\MainController::contact' => array(
+  'controllers\\MainController::partners' => array(
     array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "MainController/contact/{message}","methods"=>["get"])
   ),
-  'controllers\\MainController::partners' => array(
+  'controllers\\MainController::sendMessage' => array(
     array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "MainController/partners","methods"=>["get"])
   ),
   'controllers\\MainController::partnerDetail' => array(
-    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "MainController/partnerDetail","methods"=>["get"])
-  ),
-  'controllers\\MainController::sendMessage' => array(
     array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "MainController/sendMessage","methods"=>["post"])
   ),
 );
--- td2/app/controllers/MainController.php
+++ td2/app/controllers/MainController.php
@@ -8,41 +8,48 @@ use ws\controllers\AbstractWsController;
 class MainController extends AbstractWsController{
     
 
-public function index() {
-    $this->loadView("MainController/index.html");
+    public function index() {
+        $menu=$this->getMenu('Home');
+        $messages=$this->dataProvider->getMessages();
+        $content=$this->dataProvider->getPageContent('Home');
+        var_dump($menu);
+        var_dump($messages);
+        var_dump($content);
 /**
 *
 * @get("_default","name"=>"Home")
 */
 }
-        
-    
-public function contact($message,$who='world'){
-    $this->loadView('MainController/contact.html');
+
+          
+    public function contact($message,$who='world'){
+        $this->loadView('MainController/contact.html');
 /**
 *@route("MainController/contact/{message}","methods"=>["get"])
 **/
 }
 
 
-public function partners(){
-    $this->loadView('MainController/partners.html');
+    public function partners(){
+        $menu=$this->getMenu('Partners');
+        $messages=$this->dataProvider->getPartners();
+        $this->loadView('main/vBase.html');
 /**
  *@route("MainController/partners","methods"=>["get"])
 **/
 }
 
 
-public function sendMessage(){
-    $this->loadView('MainController/sendMessage.html');
+    public function sendMessage(){
+        $this->loadView('MainController/sendMessage.html');
 /**
 *@route("MainController/sendMessage","methods"=>["post"])
 **/
 }
 
 
-public function partnerDetail($MainControllerpartnerDetails){
-    $this->loadView('MainController/partnerDetail.html');
+    public function partnerDetail($MainControllerpartnerDetails){
+        $this->loadView('MainController/partnerDetail.html');
 /**
 *@route("MainController/partnerDetail","methods"=>["get"])
 **/
