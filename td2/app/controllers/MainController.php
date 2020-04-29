6e7f3ead1a725b5549473b8cbe7b59d362d36317
Thibault716 <62308553+Thibault716@users.noreply.github.com>
2020-04-29 16:52:33 +0200

    TD

--- td2/app/controllers/MainController.php
+++ td2/app/controllers/MainController.php
@@ -8,16 +8,16 @@ use ws\controllers\AbstractWsController;
 class MainController extends AbstractWsController{
     
 
+    /**
+     *
+     * @get("_default","name"=>"Home")
+     */
     public function index() {
         $menu=$this->getMenu('Home');
         $messages=$this->dataProvider->getMessages();
         $content=nl2br($this->dataProvider->getPageContent('Home'));
         $this->loadView('MainController/index.html',compact('messages','content') + $menu);
-/**
-*
-* @get("_default","name"=>"Home")
-*/
-}
+    }
 
           
     public function contact($message,$who='world'){
