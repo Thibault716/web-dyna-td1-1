5d91cba2ceb09e01754f78cac88e2d2d46c20ba4
Thibault716 <62308553+Thibault716@users.noreply.github.com>
2020-05-12 20:32:30 +0200

    tp noté

--- tp3/app/models/User.php
+++ tp3/app/models/User.php
@@ -64,7 +64,7 @@ class User{
 	}
 
 	 public function getFirstname(){
-		return $this->firstname.' '.$this->lastname;
+		return $this->firstname;
 	}
 
 	 public function setFirstname($firstname){
@@ -121,6 +121,7 @@ class User{
 
 	 public function __toString(){
 		return ($this->email??'no value').'';
+		return $this->firstname.' '.$this->lastname;
 	}
 
 }
